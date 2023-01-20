d7664b42de0afb3637fd2c169445b5e6657755c4
latz09 <85132328+latz09@users.noreply.github.com>
2023-01-20 15:52:22 -0600

    d

--- pages/articles/ancient-apocalypse/ancient-myths/greatFloods.js
+++ pages/articles/ancient-apocalypse/ancient-myths/greatFloods.js
@@ -1,5 +1,5 @@
 import Article from "../../../../components/articles/Article";
-import { greatFloods } from "../../../../data/articles/GreatFloods";
+import { greatFloods } from "../../../../data/articles/greatFloods";
 
 const greatFloodPage = () => {
 	return (
