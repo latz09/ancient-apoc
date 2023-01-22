94715879758e1a22cd9818159953afc20cc6bbfc
latz09 <85132328+latz09@users.noreply.github.com>
2023-01-22 15:22:54 -0600

    scales

--- components/articles/ArticleCard.js
+++ components/articles/ArticleCard.js
@@ -4,7 +4,7 @@ const ArticleCard = ({ title, tease, link }) => {
 	return (
 		<>
 			<Link href={link}>
-				<div className='cursor-pointer grid gap-4 mx-4'>
+				<div className='cursor-pointer grid gap-4 mx-4 md:scale-95 md:hover:scale-105 transition duration-700'>
 					<div className='text-xl lg:text-3xl font-bold '>{title}</div>
 					<div className="md:text-xl font-semibold opacity-50">{tease}</div>
 				</div>
